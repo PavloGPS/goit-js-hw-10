918e2a5e82cf6e035a46eb0f4045205d6d8e0e02
Pavlo Grigoriev <gpavlo2020@gmail.com>
2023-04-24 19:27:24 +0300

    Update fetchCountries.js
    filtrated fetch applied? by using fields=

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,7 @@
 export function fetchCountries(name) {
   const BASE_URL = 'https://restcountries.com/v3.1';
   const END_POINT_NAME = '/name';
-  const FILTER='name.official,capital,population,flags.svg,languages'
+  const FILTER='fields=name.official,capital,population,flags.svg,languages'
 
   console.log(name);
   // return fetch("https://jsonplaceholder.typicode.com/users").then(
