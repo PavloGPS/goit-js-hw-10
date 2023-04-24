668d0edf02d40f26fc2d6403c1f7a47e21551c2e
Pavlo Grigoriev <gpavlo2020@gmail.com>
2023-04-24 13:20:50 +0300

    first step to fetch Countries

---
+++ src/fetchCountries.js
@@ -0,0 +1,14 @@
+export function fetchCountries(name) {
+  const BASE_URL = 'https://restcountries.com/v3.1';
+  const END_POINT_NAME = '/name';
+  const FILTER='name.official,capital,population,flags.svg,languages'
+
+  console.log(name);
+  // return fetch("https://jsonplaceholder.typicode.com/users").then(
+  return fetch(`${BASE_URL}${END_POINT_NAME}/${name}?${FILTER}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './css/styles.css';
-  
+import { fetchCountries } from "./fetchCountries.js";
+import debounce from 'lodash.debounce';
+
 const DEBOUNCE_DELAY = 300;
+// const BASE_URL="https://restcountries.com/v3.1";
+// const END_POINT_NAME= "/name"
+const textInputEl=document.querySelector("#search-box");
+const countryListEl=document.querySelector(".country-list");
+const countryInfoEl=document.querySelector(".country-info");
+let name="";
+
+textInputEl.addEventListener(
+    'input',
+    debounce(evt => {
+      name = textInputEl.value;
+      console.log(fetchCountries(name));      
+    }, DEBOUNCE_DELAY)
+  );
