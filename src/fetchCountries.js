bffbbe29c6005982958c39af99b5caa62d096c83
Pavlo Grigoriev <gpavlo2020@gmail.com>
2023-04-25 13:54:07 +0300

    step 2
    adds a lot of code

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,11 +1,14 @@
 export function fetchCountries(name) {
   const BASE_URL = 'https://restcountries.com/v3.1';
   const END_POINT_NAME = '/name';
-  const FILTER='fields=name.official,capital,population,flags.svg,languages'
 
+  const params = new URLSearchParams({
+    fields:"name,capital,population,flags,languages",    
+  });
+   
   console.log(name);
   // return fetch("https://jsonplaceholder.typicode.com/users").then(
-  return fetch(`${BASE_URL}${END_POINT_NAME}/${name}?${FILTER}`).then(response => {
+  return fetch(`${BASE_URL}${END_POINT_NAME}/${name}?${params}`).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,68 @@
 import './css/styles.css';
-import { fetchCountries } from "./fetchCountries.js";
+import { fetchCountries } from './fetchCountries.js';
 import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
 // const BASE_URL="https://restcountries.com/v3.1";
 // const END_POINT_NAME= "/name"
-const textInputEl=document.querySelector("#search-box");
-const countryListEl=document.querySelector(".country-list");
-const countryInfoEl=document.querySelector(".country-info");
-let name="";
+const textInputEl = document.querySelector('#search-box');
+const countryListEl = document.querySelector('.country-list');
+const countryInfoEl = document.querySelector('.country-info');
+let name = '';
 
-textInputEl.addEventListener(
-    'input',
-    debounce(evt => {
-      name = textInputEl.value;
-      console.log(fetchCountries(name));      
-    }, DEBOUNCE_DELAY)
-  );
+textInputEl.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(evt) {
+    searchName = evt.target.value.trim();
+
+    if (!searchName){
+        countryListEl.innerHTML='';
+        countryInfoEl.innerHTML='';
+        return;
+    }
+
+    fetchCountries(searchName)
+    .then(countriesArr=>{
+        if(countriesArr.length>10){
+            Notify.info("Too many matches found. Please enter a more specific name.");
+        throw new Error('matchesOverflowError');
+    }
+    return countriesArr;
+    })
+    .then(countriesArr=>{
+        if(countriesArr.length>1 && countriesArr.length<11){
+           renderCountriesList(countriesArr); 
+        }
+        return countriesArr;
+    })
+    .then(countriesArr=>{
+        if(countriesArr.length === 1){
+           renderCountrieInfo(countriesArr); 
+        }
+    })
+    .catch(error=>{
+        if(error.message==='matchesOverflowError'){            
+        }else{
+            onFetchCountriesError()
+        }
+    })
+
+    console.log(fetchCountries(name));
+  }
+  function onFetchCountriesError() {
+    Notify.failure("Oops, there is no country with that name");
+  }
+  function renderCountriesList() {
+    //to do render of markup countries list
+  }
+  function createCountryListMarkup() {
+    //to do markup of countries list
+  }
+  
+  function renderCountrieInfo() {
+    //to do render of markup countrie info
+  }
+  function createCountryInfoMarkup() {
+    //to do markup of countrie info
+  }
